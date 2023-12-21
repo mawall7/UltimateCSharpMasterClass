afa5973fa762f622225d88d5ce6aedfe2cb1af9f
Mattias Wallerman <wallermanmattias@yahoo.se>
2023-12-21 12:20:57 +0100

    Kommentarer

--- CustomCache/AssignmentStartupFile.cs
+++ CustomCache/AssignmentStartupFile.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Se även beskrivning i Program. Genom Funct parametern i Get metoden kan flera metoder med gemensamt interface köras genom CustomCache klassen som kör metoden Download (i det här fallet när data inte cachas). Man kan på det här sättet köra flera implementationer av Downloadklassen. Download i parametern som körs på en instans av PrintDataDownload, kör i sin tur i sin Download metod en Download metod av sin instans av en SlowDataDownloader. Så klassen kan byggas ut genom att använda en klass som wrapper för en annan så att klassen SlowDataDownloader byggs ut genom att lägga till nya beteenden. 
+
+using System;
 using System.Collections.Generic;
 using System.Threading;
 namespace CustomCache {
@@ -15,14 +17,14 @@ public interface IDataDownloader
         private readonly IDataDownloader _dataDownloader;
         private readonly CustomCache<string, string> _cache  = new();
 
-        public CashingDataDownloader(IDataDownloader downloader)
+        public CashingDataDownloader(IDataDownloader downloader)  //Program  new PrintingDataDownloader(new SowDataDwonloader)
         {
             _dataDownloader = downloader;
         }
 
         public string DownloadData(string resourceId)
         {
-            return _cache.GetData(resourceId, _dataDownloader.DownloadData);
+            return _cache.GetData(resourceId, _dataDownloader.DownloadData); //Se även beskrivning i Program. Genom Funct parametern i Get metoden kan flera metoder med gemensamt interface köras genom CustomCache klassen som kör metoden Download (i det här fallet när data inte cachas). Man kan på det här sättet köra flera implementationer av Downloadklassen. Download i parametern som körs på en instans av PrintDataDownload, kör i sin tur i sin Download metod en Download metod av sin instans av en SlowDataDownloader. Så klassen kan byggas ut genom att använda en klass som wrapper för en annan så att klassen SlowDataDownloader byggs ut genom att lägga till nya beteenden. 
         }
     }
 
--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Genom Open-Closed Principle kan vi bygga ut SlowDataDownloader klassen genom att lägga till ny klasser istället för att förändra SlowDataDownloaderklassen. (CustomCache.Get kollar att cachen inte redan innehåller data) Varje ny Downloader kör dels sin egen metod dels en annan Downloader metod genom att ta en annan downladerklass instans i sin konstruktor genom Dependency Injection. 
+//I Program rad 16 kommer metoden CashingDataDownloader.DownloadData 1. Att gå till CustomCashe GetData som tar någon Downloadmetod (Funct parameter). 2. Klassen i exemplet tar en PrintingDatadownloader och dess konstruktor tar en SlowDataDownloader. Därför kommer först CashingDataDownloaders DataDownload metoden att köras och 2. Genom CustomCash Get metoden (På rad 19 i CustomCache,
+//var data = getDataOnFirstTry)) kommer den här klassens Downloader metod köra Get metoden med den här instansens downloader som Funct parameter, vilket är en PrintingDataDownloader instans i exemplet i Program: new CashinDataDownloader(new PrintingDataDownloader(..). PrintingDatadownloaderns Download metod körs som i sin tur tar en instans av SlowDataDownloader, så att den den klassens
+//Downloader metod också körs. Alla klassers Downloader metoder kommer alltså att köras genom att vi tar Downloader som en Funt parameter. Där varje Downloader gör något kan vi få en både Cashing och ha Decoratorklasser som bygger ut klassen genom att använda interfacet.  
+
+
+using System;
 
 namespace CustomCache
 {
