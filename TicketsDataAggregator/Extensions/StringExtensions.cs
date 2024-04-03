a4d896695a2952721a076db2ec542fa4baf858a7
Mattias Wallerman <wallermanmattias@yahoo.se>
2024-04-03 19:03:02 +0200

    - Implement basic ticketdataaggregation methods and classes for substracting ticket data from a file.

---
+++ TicketsDataAggregator/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsDataAggregator.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveSubstring( this string source, string target)
+        {
+
+            int index = source.IndexOf(target);
+            string cleanString = (index < 0)
+                ? source
+                : source.Remove(index, target.Length);
+
+            return cleanString;
+        }
+
+       
+        public static string ReturnsSubstringFromIndex(this string source , int startIndex, int endIndex)
+        {
+            int length = endIndex;
+            return source.Substring(startIndex, length);
+        }
+
+    }
+}
---
+++ TicketsDataAggregator/TicketDataDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsDataAggregator
+{
+    public class TicketDataDTO
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("{0, -25} {1, -25} {2, -25}", Title,  "|" + Date, "|" + Time);
+            
+        }
+
+    }
+    
+    
+        
+
+}
