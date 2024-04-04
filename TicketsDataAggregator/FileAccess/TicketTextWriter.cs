0e2437244a9b0cabab5d9cfde51ead6e685482bd
Mattias Wallerman <wallermanmattias@yahoo.se>
2024-04-04 17:41:09 +0200

    - creates class that writes the ticketdata to Text. - list erased since it's not needed.

---
+++ TicketsDataAggregator/FileAccess/TicketTextWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace DataAccess
+{
+    internal class TicketTextWriter
+    {
+        private string _filenamepath;
+
+        public TicketTextWriter(string filenamepath)
+        {
+            _filenamepath = filenamepath;
+        }
+
+        internal void AddToFile(string ticketdata)
+        {
+            using (StreamWriter writer = File.AppendText(_filenamepath))
+            {
+                writer.WriteLine(ticketdata);
+            }
+        }
+    }
+}
