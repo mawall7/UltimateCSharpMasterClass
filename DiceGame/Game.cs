508669cf8132dddecb34a20d1547645e7184cd78
Mattias Wallerman <wallermanmattias@yahoo.se>
2024-04-19 13:18:04 +0200

    Refactor to Separate ReadMethod

--- DiceGame/Game.cs
+++ DiceGame/Game.cs
@@ -6,31 +6,77 @@ namespace DiceGame
     {
         public readonly Dice _dice;
 
+        private int _tries = 3;
+
         public int _result { get; set; }
         public Game(Dice dice)
         {
             _dice = dice;
         }
 
-        public void Start()
+        //public void PlayDice()
+        //{
+        //    _result = _dice.Roll();
+        //    Console.WriteLine($"Dice rolled. Guess what number it shows in {_tries} tries.");
+
+        //}
+
+        //public bool Guess() //Refactor to Separate ReadMethod
+        //{
+
+        //    int guess = default;
+        //    string guesstring = default;
+
+        //    Console.WriteLine("Make a guess");
+        //    do
+        //    {
+        //        guesstring = Console.ReadLine();
+        //    }
+        //    while (int.TryParse(guesstring, out guess) == false);
+
+        //    return guess.Equals(_result) ? true : false;
+        //}
+
+        public bool IsRightGuess(int guess)
+        {
+            return guess.Equals(_result) ? true : false;
+        }
+        public GameResult Play()
         {
             _result = _dice.Roll();
-        }
+            Console.WriteLine($"Dice rolled. Guess what number it shows in {_tries} tries.");
 
-        public bool Guess()
-        {
-            int guess = default;
-            string guesstring = default;
-            
-            Console.WriteLine("Make a guess");
-           
-            do
+            while (_tries > 0)
             {
-                guesstring = Console.ReadLine();
-            } 
-            while(int.TryParse (guesstring, out guess) == false );
 
-            return guess.Equals(_result)? true : false;
+                //var guess = Guess();
+                var guess = ConsoleReader.ReadInteger("Enter a number");
+                _tries--;
+
+                if (IsRightGuess(guess))
+                {
+                    
+                    return GameResult.Victory;
+                }
+                else 
+                {
+                    Console.WriteLine("Wrong number");
+                }
+            }
+                return GameResult.Loss;
         }
+        
+        internal static void PrintResult(GameResult gameResult)
+        {
+            string message = gameResult == GameResult.Victory ? "You win" : "You loose";
+            Console.WriteLine(message);
+        }
+        
+                    
+
+            
     }
 }
+
+
+
--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -7,39 +7,44 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
-            int tries = 3;
+            //int tries = 3; // move to gamesession class
             Dice dice = new(new Random());
             Game gamesession = new Game(dice);
-            
-            gamesession.Start();
-     
+          
+            GameResult gameResult = gamesession.Play();
+            Game.PrintResult(gameResult);
 
-            while (tries > 0 ) 
-            {
+            Console.ReadKey();
 
-                var guess = gamesession.Guess();
-                tries--;
-                
-                if (guess == true)
-                {
-                    Console.WriteLine("You win");
-                    break;
-                }
-                if (guess == false)
-                {
-                    Console.WriteLine("Wrong number");
-                    if (tries == 0)
-                    {
-                        Console.WriteLine("You Lose");
-                    }
-                }
-            }
-                    
-            Console.ReadLine();
+
+
+
+            //    while (tries > 0 ) 
+            //    {
+
+            //        var guess = gamesession.Guess();
+            //        tries--;
+
+            //        if (guess == true)
+            //        {
+            //            Console.WriteLine("You win");
+            //            break;
+            //        }
+            //        if (guess == false)
+            //        {
+            //            Console.WriteLine("Wrong number");
+            //            if (tries == 0)
+            //            {
+            //                Console.WriteLine("You Lose");
+            //            }
+            //        }
+            //    }
+
+            //    Console.ReadLine();
+            //}
         }
     }
 }
-
             
                
                 
